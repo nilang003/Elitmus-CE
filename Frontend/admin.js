aee32dc81f89fb07afa1c2670ec20c00f3da6c5c
Nilang Thakker <nilangthakker59@gmail.com>
2023-02-13 15:33:44 +0530

    retrieve image from backend to show on dashboard

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -23,3 +23,36 @@ fetch('http://localhost:5000/api/user', {
     });
 
 
+const image = document.getElementById('image');
+
+fetch('http://localhost:5000/api/products', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[data.length - 1].image);
+
+        for (const i of data.reverse()) {
+            const img = new Image();
+            img.src = `C:\\Users\\nktha\\Desktop\\Backend\\Backend\\${i.image}`
+            const div = document.createElement('div');
+
+            // Create a timestamp element for each image
+            const timestamp = document.createElement('p');
+            timestamp.textContent = new Date(i.timeStamp).toLocaleString();
+
+            // Add the image and timestamp to the div
+            div.appendChild(img);
+            div.appendChild(timestamp);
+            document.body.appendChild(div);
+        }
+        // image.src = `C:\\Users\\nktha\\Desktop\\Backend\\Backend\\${data[data.length - 1].image}`;
+        console.log('Data submitted successfully');
+    })
+    .catch(error => {
+        console.error(error);
+        console.log('An error occurred while submitting the data');
+    });
