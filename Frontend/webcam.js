5e5fdd44efe42d28d784bc62026cbbc93723a2ce
Nilang Thakker <nilangthakker59@gmail.com>
2023-02-13 15:29:04 +0530

    sends formData to Backend

--- Frontend/webcam.js
+++ Frontend/webcam.js
@@ -1,96 +1,61 @@
+function base64ToBlob(base64) {
+  // Split the base64 string into data and contentType
+  const [, data] = base64.split(',');
+  const contentType = 'image/png';
+  console.log(contentType);
+  // Decode the base64 data
+  const binaryData = atob(data);
+
+  // Create a typed array from the binary data
+  const uint8Array = new Uint8Array(binaryData.length);
+  for (let i = 0; i < binaryData.length; i++) {
+    uint8Array[i] = binaryData.charCodeAt(i);
+  }
+  // Return the Blob object
+  return new Blob([uint8Array], { type: contentType });
+}
+
+
 const video = document.getElementById('videoElement');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const BITLY_ACCESS_TOKEN = "4ceb58c78a30d28a1feffd40cc34c6b21626e4b7";
 
 navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-        video.play();
-    })
-    .catch(err => {
-        console.error(`An error occurred: ${err}`);
-    });
+  .then(stream => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(err => {
+    console.error(`An error occurred: ${err}`);
+  });
 
 let click = function () {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const uri = canvas.toDataURL(0.1);
-    const date = Date.now();
-    let ans = [uri, date]
-    return ans;
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const uri = canvas.toDataURL(0.01);
+  const date = Date.now();
+  let ans = [uri, date]
+  return ans;
 };
 
 
-async function shortenUrl(url) {
-    const response = await fetch(`https://api-ssl.bitly.com/v4/shorten`, {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${BITLY_ACCESS_TOKEN}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        long_url: url
-      })
-    });
-    const json = await response.json();
-    return json.link;
-  }
-
-
-setInterval(() => {
-    let img = click();
-    let uri = img[0]
-    let timeStapmp = img[1]
-    let shorturi = shortenUrl(uri);
-    console.log(shorturi)
-    //console.log(uri);
-    // console.log(timeStapmp);
-    const data = { timeStapmp, productImage: uri }
-    fetch('http://localhost:5000/api/products', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            console.log('Data submitted successfully');
-        })
-        .catch(error => {
-            console.error(error);
-            console.log('An error occurred while submitting the data');
-        });
-}, 1 * 60 * 1000);
-
-
-const longUrl = "https://example.com/image.jpg";
-shortenUrl(longUrl)
-  .then(shortUrl => {
-    console.log(shortUrl);
-  })
-  .catch(error => {
-    console.error(error);
+const takePhoto = async () => {
+  const [uri, date] = click();
+  const blob = base64ToBlob(uri);
+  console.log(blob);
+  const formData = new FormData();
+  formData.append('productImage', blob, `im.png`);
+  formData.append('timeStamp',date);
+  const response = await fetch('http://localhost:5000/api/products', {
+    method: 'POST',
+    body: formData
   });
 
+  if (!response.ok) {
+    console.error('Failed to send photo to API');
+  }
+}
 
-
-
-// fetch('http://localhost:5000/api/product', {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//             console.log('Data submitted successfully');
-//         })
-//         .catch(error => {
-//             console.error(error);
-//             console.log('An error occurred while submitting the data');
-//         });
+// Take a photo at a continuous interval
+setInterval(takePhoto, 5000);
